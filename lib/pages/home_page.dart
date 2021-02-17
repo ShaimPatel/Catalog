128b6a27d38242de6e85527764ae032d0df335ab
Shivam Patel <sp5036018@gmail.com>
2021-02-17 22:11:10 +0530

    day 13 is Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,28 @@ import 'package:day_thirty_flutter/models/catalog.dart';
 import 'package:day_thirty_flutter/pages/drawer_page.dart';
 import 'package:day_thirty_flutter/pages/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    final productdata = decodedData("products");
+    print(productdata);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
@@ -17,6 +37,7 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
+        //scrollDirection: Axis.horizontal,
         // itemCount: CatalogModel.items.length,
         itemCount: dummyList.length,
         itemBuilder: (context, index) {
